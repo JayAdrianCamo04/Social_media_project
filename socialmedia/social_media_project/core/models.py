6f8422e2bce4f30669e37d2f57230f11b7e1469a
JayAdrianCamo04 <jayvillenajayvillena04@gmail.com>
2023-01-27 12:13:52 +0800

    second commit camo

--- socialmedia/social_media_project/core/models.py
+++ socialmedia/social_media_project/core/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+
+User = get_user_model()
+# Create your models here.
+class Profile(models.Model):
+    user = models.Foreignkey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture')
+    location = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.user.username
